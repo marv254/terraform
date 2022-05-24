70eefcd630184df384118590905178e6d3a2e6c3
Marv_K254 <marvinkorir.mk@gmail.com>
2022-05-24 12:43:20 +0300

    used provisioners

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ variable env_prefix{}
 variable my_ip{}
 variable instance_type{}
 variable public_key_location{}
+variable private_key_location{}
 
 resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
@@ -110,7 +111,23 @@ resource "aws_instance" "myapp-server"{
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("entry-script.sh")
+    # user_data = file("entry-script.sh")
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_location)
+    }
+
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "/home/ec2-user/entry-scrit.sh"
+    } 
+
+    provisioner "remote-exec" {
+        script = "entry-script.sh"
+    }
  
     tags = {
         Name = "${var.env_prefix}-server"
